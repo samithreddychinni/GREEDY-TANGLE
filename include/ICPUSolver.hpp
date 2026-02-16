a5267ecd9a499cf002bd7e9e478f3d19695d1240
Mohith Kumar <mohithk611@gmail.com>
2026-02-16 17:16:54 +0530

    feat: define ICPUSolver interface with solvermode enum

---
+++ include/ICPUSolver.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "GraphData.hpp"
+#include <string>
+#include <vector>
+
+namespace GreedyTangle {
+
+struct CPUMove;
+
+enum class SolverMode {
+  GREEDY,                  
+  DIVIDE_AND_CONQUER_DP    
+};
+
+
+class ICPUSolver {
+public:
+  virtual ~ICPUSolver() = default;
+
+  
+  virtual CPUMove FindBestMove(std::vector<Node> nodes,
+                               const std::vector<Edge> &edges) = 0;
+
+  
+  virtual std::string GetName() const = 0;
+
+  
+  virtual int GetLastCandidatesEvaluated() const = 0;
+};
+
+}
